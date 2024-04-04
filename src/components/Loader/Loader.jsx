22e6b72db18bac2966bb1c9f9adcc0fb985c2dc3
Yaroslava <yaroslava.hovenko@gmail.com>
2024-04-04 11:49:22 +0300

    fix style

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,21 @@
 import { RotatingLines } from "react-loader-spinner";
+import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <RotatingLines
-      visible={true}
-      height="75"
-      width="75"
-      strokeColor="grey"
-      strokeWidth="2"
-      animationDuration="0.75"
-      ariaLabel="rotating-lines-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-    />
+    <div className={css.loaderWrap}>
+      <RotatingLines
+        visible={true}
+        height="75"
+        width="75"
+        strokeColor="grey"
+        strokeWidth="2"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
   );
 };
 
