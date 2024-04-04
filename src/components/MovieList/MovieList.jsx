f4b464f7bb40e3cfa37c1860b8cee6207aeab393
Yaroslava <yaroslava.hovenko@gmail.com>
2024-04-04 16:41:15 +0300

    fix

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,16 +1,24 @@
 import { Link, useLocation } from "react-router-dom";
+import css from "./MovieList.module.css";
 
 const MovieList = ({ movies }) => {
   const location = useLocation();
   return (
-    <ul>
-      {movies.map((item) => (
-        <li key={item.id}>
-          <Link state={location} to={`/movies/${item.id}`}>
-            {item.original_title}
-          </Link>
-        </li>
-      ))}
+    <ul className={css.movieWrap}>
+      {movies !== null &&
+        movies.map((item) => (
+          <li className={css.movieItem} key={item.id}>
+            <Link state={location} to={`/movies/${item.id}`}>
+              <img
+                className={css.movieImg}
+                src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                alt={item.original_title}
+              />
+              <p className={css.movieTitle}> {item.original_title}</p>
+              <div className={css.movieOverlay}></div>
+            </Link>
+          </li>
+        ))}
     </ul>
   );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import Loader from "../../components/Loader/Loader";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 
 import css from "./HomePage.module.css";
+import MovieList from "../../components/MovieList/MovieList";
 
 const HomePage = () => {
   const [movies, setMovies] = useState(null);
@@ -33,23 +34,7 @@ const HomePage = () => {
     <div className={css.homeWrap}>
       {loading && <Loader />}
       {error && <ErrorMessage />}
-      {movies !== null && (
-        <ul className={css.movieWrap}>
-          {movies.map(({ title, id, poster_path }) => (
-            <li className={css.movieItem} key={id}>
-              <Link className={css.movieLink} to={`/movies/${id}`}>
-                <img
-                  className={css.movieImg}
-                  src={`https://image.tmdb.org/t/p/w300${poster_path}`}
-                  alt={title}
-                />
-                <p className={css.movieTitle}>{title}</p>
-                <div className={css.movieOverlay}></div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
+      <MovieList movies={movies} />
     </div>
   );
 };
